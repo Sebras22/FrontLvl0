83df19a3eb02a8f18dafcd017d3b35a56adb6ef5
Sebras22 <sebastien.branly@efrei.net>
2024-09-17 17:18:25 +0200

    FirstPushFront

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import task from "./task.vue";
+const myObject = {
+    title: "How to do lists in Vue",
+    author: "Jane Doe",
+    publishedAt: "2016-04-10",
+};
+</script>
+
+<template>
+    <h1>Todolist</h1>
+    <task name="haha" description="qzjdhlqubde"></task>
+
+    <li v-for="value in myObject">
+        {{ value }}
+    </li>
+</template>
+
+<style scoped></style>
---
+++ src/task.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    name: String,
+    description: String,
+});
+</script>
+<template>
+    <h3>{{ name }}</h3>
+    <h4>{{ description }}</h4>
+</template>
+<style scoped></style>
